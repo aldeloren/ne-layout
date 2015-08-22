378917457f7166fe08ad755244f4986c052cb818
Anthony DeLorenzo <a.ldeloren@gmail.com>
2015-08-22 11:25:16 -0400

    initalize repo, add basic grunt settings

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function( grunt ) {
+
+  grunt.initConfig({
+    sass: {
+      dist: { 
+        options: {
+          style: 'expanded'
+        },
+        files: {
+          'assets/css/main.css': 'src/scss/main.scss' 
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: 'src/scss/main.scss',
+        tasks: ['sass'],
+        options: {
+          livereload: true,
+        },
+      },
+    },
+  });
+  
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  
+  grunt.registerTask('default', ['sass']);
+
+};
---
+++ README.md
@@ -0,0 +1,11 @@
+# Northeastern Homework
+
+This repository contains example(s) of a layout design as based on the supplied requirements
+
+## Branches
+
+This repository contains a few branches that demonstrate different techniques that demonstrate different techniques to accomplish the desired results. 
+
+- Flexbox 
+- HTML5 syntax
+- Responsive divs
